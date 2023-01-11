64d67d2229f1727058ec9ef95b75efd9a2325396
Ragnar Groot Koerkamp <ragnar.grootkoerkamp@gmail.com>
2023-01-11 12:58:53 +0100

    format build.rs

--- build.rs
+++ build.rs
@@ -1,31 +1,29 @@
 extern crate bindgen;
 
-
 use std::env;
-use std::path::PathBuf;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::PathBuf;
 
-// We have path to edlib, generate wrapper.h with  path to correct include 
-fn write_wrapper(wrapper_path : &PathBuf) {
+// We have path to edlib, generate wrapper.h with  path to correct include
+fn write_wrapper(wrapper_path: &PathBuf) {
     let mut inclpath = PathBuf::new();
     inclpath.push("include");
     inclpath.push("edlib.h");
     let mut incl = String::from("#include\"");
-    incl.push_str(& inclpath.to_str().unwrap());
+    incl.push_str(&inclpath.to_str().unwrap());
     incl.push_str("\"");
     // now we write file wrapper.h
     let mut f = File::create(wrapper_path).unwrap();
-    f.write_all(incl.as_bytes()).expect("cannot write wrapper.h");
+    f.write_all(incl.as_bytes())
+        .expect("cannot write wrapper.h");
 }
-
-
 
 fn main() {
     //  try to build edlib in build.rs with edlib cloned in edlib-c (from which we removed )
     let dst = cmake::Config::new("edlib-c")
-                .cflag("-D CMAKE_BUILD_TYPE=Release")
-                .build();
+        .cflag("-D CMAKE_BUILD_TYPE=Release")
+        .build();
     println!("cargo:rustc-link-search=native={}", dst.display());
     //
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
@@ -33,18 +31,17 @@ fn main() {
     //
     write_wrapper(&wrapper_path);
     // adapted fix from sam217pa. libstdc++ name varies...
-    let lib_std : &str;
+    let lib_std: &str;
     if cfg!(target_os = "macos") {
-        lib_std  = "cargo:rustc-link-lib=c++";
-    }
-    else {
+        lib_std = "cargo:rustc-link-lib=c++";
+    } else {
         lib_std = "cargo:rustc-link-lib=stdc++";
     }
-    
+
     let mut libdir = String::from("cargo:rustc-link-search=");
     libdir.push_str(out_path.to_str().unwrap());
     libdir.push_str("/lib");
-    println!("{}",libdir);
+    println!("{}", libdir);
     println!("cargo:rustc-link-lib=edlib");
     println!("{}", lib_std);
 
@@ -58,7 +55,7 @@ fn main() {
         //Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
         .parse_callbacks(Box::new(bindgen::CargoCallbacks))
-         // Finish the builder and generate the bindings.
+        // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
